1ddb3cc2bda09de70ee4bfeee6dd292fdef80a06
raxden <esthar2000@gmail.com>
2015-04-27 10:53:24 +0200

    NotificationUtils added to project

---
+++ library/src/main/java/com/raxdenstudios/commons/util/NotificationUtils.java
@@ -0,0 +1,55 @@
+package com.raxdenstudios.commons.util;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by agomez on 23/04/2015.
+ */
+public class NotificationUtils {
+
+    private static final String TAG = NavigationUtils.class.getSimpleName();
+
+    private static NotificationUtils INSTANCE = null;
+
+    private NotificationUtils() {}
+
+    private synchronized static void createInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new NotificationUtils();
+        }
+    }
+
+    public static NotificationUtils getInstance() {
+        if (INSTANCE == null) createInstance();
+        return INSTANCE;
+    }
+
+    public void sendNotification(Context context, int notificationId, int smallIcon, String contentTitle, String bigText, String contentText, String ticker) {
+        Intent intent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_ONE_SHOT);
+        sendNotification(context, pendingIntent, notificationId, smallIcon, contentTitle, bigText, contentText, ticker);
+    }
+
+    public void sendNotification(Context context, PendingIntent pendingIntent, int notificationId, int smallIcon, String contentTitle, String bigText, String contentText, String ticker) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        if (smallIcon != 0) builder.setSmallIcon(smallIcon);
+        if (Utils.hasValue(contentTitle)) builder.setContentTitle(contentTitle);
+        if (Utils.hasValue(bigText)) builder.setStyle(new NotificationCompat.BigTextStyle().bigText(bigText));
+        if (Utils.hasValue(contentText)) builder.setContentText(contentText);
+        if (Utils.hasValue(ticker)) builder.setTicker(ticker);
+        if (pendingIntent != null) builder.setContentIntent(pendingIntent);
+        sendNotification(context, notificationId, builder.build());
+    }
+
+    public void sendNotification(Context context, int notificationId, Notification notification) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        mNotificationManager.notify(notificationId, notification);
+    }
+
+}
