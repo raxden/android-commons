222d2f1acb453dd045a2af123bc04b87d3c133e7
raxden <esthar2000@gmail.com>
2015-12-02 10:11:39 +0100

    update

--- library/src/main/java/com/raxdenstudios/commons/util/BitmapUtils.java
+++ library/src/main/java/com/raxdenstudios/commons/util/BitmapUtils.java
@@ -106,22 +106,18 @@ public class BitmapUtils {
 	    int statusBarHeight = frame.top;
 	    int width = activity.getWindowManager().getDefaultDisplay().getWidth();
 	    int height = activity.getWindowManager().getDefaultDisplay().getHeight();
-
 	    Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height  - statusBarHeight);
 	    view.destroyDrawingCache();
 	    return b;
 	}
     
 	public static Bitmap drawableToBitmap (Drawable drawable) {
-	    if (drawable instanceof BitmapDrawable) {
-	        return ((BitmapDrawable)drawable).getBitmap();
-	    }
-
+		if (drawable == null) return null;
+	    if (drawable instanceof BitmapDrawable) return ((BitmapDrawable)drawable).getBitmap();
 	    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
 	    Canvas canvas = new Canvas(bitmap); 
 	    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
 	    drawable.draw(canvas);
-
 	    return bitmap;
 	}	
 	
