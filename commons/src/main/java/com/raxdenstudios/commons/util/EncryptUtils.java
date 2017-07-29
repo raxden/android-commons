9e2eb5822f384bc642f13fa9cf4cfda8263f3603
raxden <raxden.dev@gmail.com>
2017-07-29 13:49:56 +0200

    Release 3.0.11 created

--- commons/src/main/java/com/raxdenstudios/commons/util/EncryptUtils.java
+++ commons/src/main/java/com/raxdenstudios/commons/util/EncryptUtils.java
@@ -30,58 +30,64 @@ public class EncryptUtils {
 
     private static final String TAG = EncryptUtils.class.getSimpleName();
 
-    public enum AlgorithmType {SHA, SHA_1, MD5}
-        
-	public static String hashKey(String key){
-		return hashKey(key, AlgorithmType.MD5, "UTF-8");
-	}
+    public enum AlgorithmType {SHA, SHA_1, SHA_512, MD5}
 
-	public static String hashKey(String key, AlgorithmType algorithmType) {
-		return hashKey(key, algorithmType, "UTF-8");
-	}
-
-	public static String hashKey(String key, AlgorithmType algorithmType, String encoding){
-		byte[] digestBytes = getDigest(key, algorithmType, encoding);
-		return Base64.encodeToString(digestBytes, Base64.URL_SAFE|Base64.NO_WRAP);
-	}
-
-	public static String hashKeyForDisk(String key) {
-		return hashKeyForDisk(key, AlgorithmType.MD5, "UTF-8");
+    public static String hashKey(String key) {
+        return hashKey(key, AlgorithmType.MD5, "UTF-8");
     }
 
-	public static String hashKeyForDisk(String key, AlgorithmType algorithmType) {
-		return hashKeyForDisk(key, algorithmType, "UTF-8");
-	}
+    public static String hashKey(String key, AlgorithmType algorithmType) {
+        return hashKey(key, algorithmType, "UTF-8");
+    }
 
-	public static String hashKeyForDisk(String key, AlgorithmType algorithmType, String encoding){ 	
-		byte[] digestBytes = getDigest(key, algorithmType, encoding);
+    public static String hashKey(String key, AlgorithmType algorithmType, String encoding) {
+        byte[] digestBytes = getDigest(key, algorithmType, encoding);
+        return Base64.encodeToString(digestBytes, Base64.URL_SAFE | Base64.NO_WRAP);
+    }
+
+    public static String hashKeyForDisk(String key) {
+        return hashKeyForDisk(key, AlgorithmType.MD5, "UTF-8");
+    }
+
+    public static String hashKeyForDisk(String key, AlgorithmType algorithmType) {
+        return hashKeyForDisk(key, algorithmType, "UTF-8");
+    }
+
+    public static String hashKeyForDisk(String key, AlgorithmType algorithmType, String encoding) {
+        byte[] digestBytes = getDigest(key, algorithmType, encoding);
         return bytesToHexString(digestBytes);
-	}    
-    
-	private static byte[] getDigest(String key, AlgorithmType algorithmType, String encoding) {
-		byte[] digest = null;
-		try {
-			MessageDigest messageDigest = MessageDigest.getInstance(getAlgorithm(algorithmType));
-    		messageDigest.update(key.getBytes(encoding));
-    		digest = messageDigest.digest();
-		} catch (NoSuchAlgorithmException e) {
-	        Log.e(TAG, e.getMessage());
-		} catch (UnsupportedEncodingException e) {
-	        Log.e(TAG, e.getMessage());
-		}
-				
-		return digest;
-	}
-	
-	private static String getAlgorithm(AlgorithmType algorithmType){
-		switch(algorithmType){
-			case SHA:	return "SHA";
-			case SHA_1:	return "SHA-1";
-			case MD5:	return "MD5";
-			default:  	return "MD5";
-		}
-	}	
-	
+    }
+
+    private static byte[] getDigest(String key, AlgorithmType algorithmType, String encoding) {
+        byte[] digest = null;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(getAlgorithm(algorithmType));
+            messageDigest.update(key.getBytes(encoding));
+            digest = messageDigest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+        return digest;
+    }
+
+    private static String getAlgorithm(AlgorithmType algorithmType) {
+        switch (algorithmType) {
+            case SHA:
+                return "SHA";
+            case SHA_1:
+                return "SHA-1";
+            case SHA_512:
+                return "SHA-512";
+            case MD5:
+                return "MD5";
+            default:
+                return "MD5";
+        }
+    }
+
     private static String bytesToHexString(byte[] bytes) {
         // http://stackoverflow.com/questions/332079
         StringBuilder sb = new StringBuilder();
@@ -94,5 +100,5 @@ public class EncryptUtils {
         }
         return sb.toString();
     }
-    
+
 }
