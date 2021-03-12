49eb9cc9a32b9a6667a9c10df9b8fbee15ae8e6d
Ángel Gómez <raxden.dev@gmail.com>
2021-03-12 14:44:57 +0100

    extract TabLayoutMatchers

--- libraries/test-android/src/main/java/com/raxdenstudios/commons/test/android/matchers/ViewMatchers.kt
+++ libraries/test-android/src/main/java/com/raxdenstudios/commons/test/android/matchers/ViewMatchers.kt
@@ -3,38 +3,11 @@ package com.raxdenstudios.commons.test.android.matchers
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Checkable
-import androidx.test.espresso.PerformException
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
-import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import com.google.android.material.tabs.TabLayout
 import org.hamcrest.BaseMatcher
 import org.hamcrest.CoreMatchers.isA
 import org.hamcrest.Description
-import org.hamcrest.Matchers.allOf
-
-object TabLayoutMatchers {
-
-  fun selectTabAtPosition(position: Int) = object : ViewAction {
-
-    override fun getConstraints() = allOf(isDisplayed(), isAssignableFrom(TabLayout::class.java))
-
-    override fun getDescription() = "With tab at index $position"
-
-    override fun perform(uiController: UiController?, view: View?) {
-      val tabLayout = view as? TabLayout ?: return
-      val tabAtPosition = tabLayout.findTabAt(position)
-      tabAtPosition.select()
-    }
-  }
-
-  private fun TabLayout.findTabAt(position: Int): TabLayout.Tab {
-    return getTabAt(position) ?: throw PerformException.Builder()
-      .withCause(Throwable("No tab at index $position"))
-      .build()
-  }
-}
 
 object ViewMatchers {
 
