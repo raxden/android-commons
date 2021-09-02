aefb277a45bbbf56f45999f4e66afd6314d7e13c
Ángel Gómez <angelgomez@xpertai.com>
2021-09-02 13:56:00 +0200

    init binding when onCreate is called

--- libraries/android/src/main/java/com/raxdenstudios/commons/property/ActivityViewBindingDelegate.kt
+++ libraries/android/src/main/java/com/raxdenstudios/commons/property/ActivityViewBindingDelegate.kt
@@ -2,16 +2,30 @@ package com.raxdenstudios.commons.property
 
 import android.app.Activity
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 class ActivityViewBindingDelegate<T : ViewBinding>(
-  bindingClass: Class<T>
-) : ReadOnlyProperty<Activity, T> {
+  bindingClass: Class<T>,
+  private val activity: AppCompatActivity,
+) : ReadOnlyProperty<Activity, T>, DefaultLifecycleObserver {
 
   private val inflateMethod = bindingClass.getMethod("inflate", LayoutInflater::class.java)
   private var binding: T? = null
+
+  init {
+    activity.lifecycle.addObserver(this)
+  }
+
+  override fun onCreate(owner: LifecycleOwner) {
+    super.onCreate(owner)
+
+    binding ?: createBinding(activity).also { binding = it }
+  }
 
   override fun getValue(
     thisRef: Activity,
