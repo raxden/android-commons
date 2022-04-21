3665951bcd89715dffb990804b3883de06de5ef2
Ángel Gómez <raxden.dev@gmail.com>
2022-04-21 20:23:11 +0200

    detekt plugin added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
 include(":libraries:android")
 include(":libraries:android-test")
 include(":libraries:base")
