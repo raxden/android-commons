de4da0f60ee4b2d52ed114a6b2e640dc83966b27
Ángel Gómez <raxden.dev@gmail.com>
2022-04-21 20:00:17 +0200

    test-logger plugin added

--- libraries/coroutines/build.gradle.kts
+++ libraries/coroutines/build.gradle.kts
@@ -1,16 +1,8 @@
 import com.raxdenstudios.publishing.model.Coordinates
-import com.adarshr.gradle.testlogger.theme.ThemeType
-
 plugins {
   id("com.raxdenstudios.android-versioning")
   id("com.raxdenstudios.android-library")
   id("com.raxdenstudios.publish-library")
-  id("com.adarshr.test-logger")
-}
-
-testlogger {
-  theme = ThemeType.MOCHA
-  slowThreshold = 3000
 }
 
 versioning {
