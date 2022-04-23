7d2e7dcde159ef52bff51c37031f9432c799bdb0
Ángel Gómez <raxden.dev@gmail.com>
2022-04-23 21:31:27 +0200

    update gradle configuration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,12 +5,8 @@ plugins {
 repositories {
   mavenCentral()
   google()
-  jcenter()
 }
 
 dependencies {
-  implementation("com.android.tools.build:gradle:4.1.1")
-  implementation(kotlin("gradle-plugin", version = "1.4.10"))
-  implementation(gradleApi())
-  implementation(localGroovy())
+
 }
