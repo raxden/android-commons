193419e0ce3bf3de9b8160552ca5e190888b8c37
Ángel Gómez <raxden.dev@gmail.com>
2022-07-10 14:30:46 +0200

    improve configuration

--- libraries/retrofit-co/src/main/java/com/raxdenstudios/commons/retrofit/NetworkResponseExtension.kt
+++ libraries/retrofit-co/src/main/java/com/raxdenstudios/commons/retrofit/NetworkResponseExtension.kt
@@ -9,10 +9,12 @@ inline fun <T : Any, U : Any, reified R : Any> NetworkResponse<T, U>.toResultDat
   transform: (value: T) -> R = { value -> value as R }
 ): ResultData<R> = when (this) {
   is NetworkResponse.Success -> ResultData.Success(transform(body))
-  is NetworkResponse.ServerError -> when (code) {
-    in 400..499 -> ResultData.Error(NetworkException.Client(code, errorMessage))
-    in 500..599 -> ResultData.Error(NetworkException.Server(code, errorMessage))
-    else -> ResultData.Error(NetworkException.Unknown(errorMessage))
+  is NetworkResponse.ServerError -> {
+    when (val code = code ?: -1) {
+      in (400..499) -> ResultData.Error(NetworkException.Client(code, errorMessage))
+      in (500..599) -> ResultData.Error(NetworkException.Server(code, errorMessage))
+      else -> ResultData.Error(NetworkException.Unknown(errorMessage))
+    }
   }
   is NetworkResponse.NetworkError -> ResultData.Error(NetworkException.Network(errorMessage, error))
   is NetworkResponse.UnknownError -> ResultData.Error(NetworkException.Unknown(errorMessage, error))
