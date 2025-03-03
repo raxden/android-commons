c299f077daa909a7caed4bc0f98f3eed500cca5c
Ángel Gómez <raxden.dev@gmail.com>
2025-03-03 20:11:30 +0100

    migrated to ksp

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
     implementation(libs.kotlin.compose.gradlePlugin)
+    implementation(libs.kotlin.serialization.gradlePlugin)
     implementation(libs.rootcoverage.gradlePlugin)
     implementation(libs.detekt.gradlePlugin)
+    implementation(libs.ksp.gradlePlugin)
 }
