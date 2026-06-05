5a8b4542d76b035cbdd3d877a501ba9ce876fd6d
Ángel Gómez <raxden.dev@gmail.com>
2026-06-05 16:28:25 +0200

    added more extensions

---
+++ libraries/core/src/main/java/com/raxdenstudios/commons/core/ext/EncryptExtension.kt
@@ -0,0 +1,48 @@
+package com.raxdenstudios.commons.core.ext
+
+import java.math.BigInteger
+import java.nio.charset.Charset
+import java.security.MessageDigest
+
+@Suppress("MagicNumber")
+fun String.toMD5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')
+}
+
+fun String.toSHA256(charset: Charset = Charsets.UTF_8): String =
+    hash(this, "SHA-256", charset)
+
+fun String.toSHA512(charset: Charset = Charsets.UTF_8): String =
+    hash(this, "SHA-512", charset)
+
+private fun hash(
+    key: String,
+    algorithm: String,
+    charset: Charset,
+): String = digest(key, algorithm, charset)
+    ?.let { bytesToHexString(it) } ?: ""
+
+private fun digest(
+    key: String,
+    algorithm: String,
+    charset: Charset = Charsets.UTF_8,
+): ByteArray? = runCatching {
+    val messageDigest = MessageDigest.getInstance(algorithm)
+    messageDigest.update(key.toByteArray(charset))
+    messageDigest.digest()
+}.getOrNull()
+
+@Suppress("MagicNumber")
+private fun bytesToHexString(bytes: ByteArray): String {
+    // http://stackoverflow.com/questions/332079
+    val sb = StringBuilder()
+    for (i in bytes.indices) {
+        val hex = Integer.toHexString(0xFF and bytes[i].toInt())
+        if (hex.length == 1) {
+            sb.append('0')
+        }
+        sb.append(hex)
+    }
+    return sb.toString()
+}
